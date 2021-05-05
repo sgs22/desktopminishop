c3896022677c4c6ae33d78613beb39355688f945
sgs22 <samuel_smith@outlook.com>
2021-05-05 18:47:33 +0100

    feat: static files

--- desktopmini/settings.py
+++ desktopmini/settings.py
@@ -128,6 +128,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT = BASE_DIR / "media_cdn"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
