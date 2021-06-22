ca42f3912686e00ecf3a0d3006b2635b08689769
sgs22 <samuel_smith@outlook.com>
2021-06-22 23:06:34 +0100

    fix uplaod img bug

--- desktopmini/urls.py
+++ desktopmini/urls.py
@@ -40,8 +40,7 @@ urlpatterns = [
     #path('success/', SuccessView.as_view(), name='success'),
     path('success/', success_view, name='success'),
     path('checkout/', checkout_view, name='checkout')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
