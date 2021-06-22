8f4edcd3aa19e87211f2d0c7440f606efff037b5
sgs22 <samuel_smith@outlook.com>
2021-06-22 22:35:56 +0100

    updated allowed hosts

--- desktopmini/settings.py
+++ desktopmini/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '.desktopmini.co.uk']
 
 
 # Application definition
