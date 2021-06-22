9d464b74d70f15cc4767a2814e4d2fd6ba00919c
sgs22 <samuel_smith@outlook.com>
2021-06-22 22:24:13 +0100

    collectstatic

---
+++ staticfiles/README.md
@@ -0,0 +1,14 @@
+# desktopminishop - Django / Stripe Payments
+
+### System Integrator Website selling a premium desktop computer, built using django python framework and stripe for order/checkout 
+
+### TODO:
+    - Add media
+    - Edit content
+
+### Preview:
+#### Index:
+![Preview1](/Screenshot%202021-05-06%20174953.png)
+#### Features:
+![Preview2](/Screenshot%202021-05-06%20174935.png)
+
---
+++ staticfiles/desktopmini/urls.py
@@ -0,0 +1,47 @@
+"""desktopmini URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from django.conf import settings
+from django.conf.urls.static import static 
+
+from shop.views import (
+    shop_view,
+    product_overview,
+    about_view,
+    support_view,
+    SuccessView,
+    success_view,
+    CancelView,
+    checkout_view
+)
+
+urlpatterns = [
+    path('', shop_view, name='index'),
+    path('desktopmini/', product_overview, name='overview'),
+    path('about/', about_view, name='about'),
+    path('support/', support_view, name='support'),
+    path('cpanel/', admin.site.urls),
+    path('cancel/', CancelView.as_view(), name='cancel'),
+    #path('success/', SuccessView.as_view(), name='success'),
+    path('success/', success_view, name='success'),
+    path('checkout/', checkout_view, name='checkout')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
