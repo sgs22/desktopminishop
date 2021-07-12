f790e79e3cdb6ce21ec1e14419bc074bbceb4c9d
sgs22 <samuel_smith@outlook.com>
2021-07-12 17:45:11 +0100

    rm cloudinary from installed apps

--- desktopmini/settings.py
+++ desktopmini/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary',
-    'cloudinary_storage',
     'shop',
 ]
 
