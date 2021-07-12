9679d118361d91606daef0d211f9372fd18f614b
sgs22 <samuel_smith@outlook.com>
2021-07-12 20:31:39 +0100

    settings

--- desktopmini/settings.py
+++ desktopmini/settings.py
@@ -140,8 +140,8 @@ MEDIA_URL= '/media/'
 MEDIA_ROOT = BASE_DIR / "media_cdn"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
